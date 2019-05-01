4135244552d290be7ad24dab2a71dcbd579acdd3
dwikykasih <kasihcheck@gmail.com>
2019-05-01 20:24:47 +0700

    fitur login

---
+++ Login/src/com/controller/controller_login.java
@@ -0,0 +1,8 @@
+package com.controller;
+
+import com.view.FormLogin;
+import java.sql.SQLException;
+
+public interface controller_login {
+    public void Login (FormLogin lg) throws SQLException;
+}
